9f7a77ee768d5b53aabb8b747930a6cd5ec5f8e6
Max <scourgetp@gmail.com>
2026-05-10 13:09:45 +0300

    [DEV] Added messages

--- mlir/compiler-course/potashnik_m_lab4/loop_trip_count.cpp
+++ mlir/compiler-course/potashnik_m_lab4/loop_trip_count.cpp
@@ -1,10 +1,15 @@
 #include "mlir/Dialect/Affine/Analysis/LoopAnalysis.h"
 #include "mlir/Dialect/Affine/IR/AffineOps.h"
 #include "mlir/Dialect/LLVMIR/LLVMDialect.h"
+#include "mlir/IR/BuiltinAttributes.h"
 #include "mlir/IR/PatternMatch.h"
 #include "mlir/Pass/Pass.h"
 #include "mlir/Tools/Plugins/PassPlugin.h"
+#include "llvm/Support/Debug.h"
 #include "llvm/Support/raw_ostream.h"
+#include <limits>
+
+#define DEBUG_TYPE "trip-count"
 
 using namespace mlir;
 
@@ -16,22 +21,30 @@ public:
   StringRef getDescription() const final { return "Adds trip_count for loops"; }
 
   void runOnOperation() override {
-    ModuleOp mod_op = getOperation();
+    ModuleOp module_op = getOperation();
 
-    mod_op.walk([&](affine::AffineForOp for_op) {
+    module_op.walk([&](affine::AffineForOp for_op) {
       std::optional<uint64_t> trip_count_opt =
           affine::getConstantTripCount(for_op);
-      if (!trip_count_opt)
+      if (!trip_count_opt) {
+        LLVM_DEBUG(llvm::dbgs()
+                   << "Non-constant trip count: " << for_op << "\n");
         return;
+      }
+
       uint64_t trip_count = *trip_count_opt;
       if (trip_count >
           static_cast<uint64_t>(std::numeric_limits<int64_t>::max())) {
+        LLVM_DEBUG(llvm::dbgs() << trip_count << " cant fit into i64\n");
         return;
       }
+
       for_op->setAttr(
           "trip_count",
           IntegerAttr::get(IntegerType::get(for_op.getContext(), 64),
                            static_cast<int64_t>(trip_count)));
+      LLVM_DEBUG(llvm::dbgs()
+                 << "Trip_count = " << trip_count << " : " << for_op << "\n");
     });
   }
 };
